ba5a67493c684e1c48e0ff8540671156110228e5
santiago granada aguirre <32211963+santix96@users.noreply.github.com>
2020-11-01 16:03:13 -0500

    basic gui setings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FULL_ROUTES} from './routes/full.routes';
+import {FullLayoutComponent} from './layouts/fullLayouts/full-layout/full-layout.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FullLayoutComponent,
+    children: FULL_ROUTES,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
